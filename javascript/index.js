08474440138284d0c0d9609e22135416c3f5fda1
taffydesu <taffyvibritannia@gmail.com>
2024-02-08 13:47:51 +0900

    added more countries and applied live time to them too

--- javascript/index.js
+++ javascript/index.js
@@ -1,14 +1,15 @@
 function updateTime() {
   //tokyo
   let tokyoElement = document.querySelector("#tokyo");
-  let tokyoDateElement = tokyoElement.querySelector(".date");
-  let tokyoTimeElement = tokyoElement.querySelector(".time");
-  let tokyoTime = moment().tz("Asia/Japan");
+  if (tokyoElement) {
+    let tokyoDateElement = tokyoElement.querySelector(".date");
+    let tokyoTimeElement = tokyoElement.querySelector(".time");
+    let tokyoTime = moment().tz("Asia/Japan");
 
-  tokyoDateElement.innerHTML = tokyoTime.format("dddd, MMMM Do YYYY");
-  tokyoTimeElement.innerHTML = tokyoTime.format("HH:mm:ss");
-
-  //manama
+    tokyoDateElement.innerHTML = tokyoTime.format("dddd, MMMM Do YYYY");
+    tokyoTimeElement.innerHTML = tokyoTime.format("HH:mm:ss");
+  }
+  //bahrain
   let manamaElement = document.querySelector("#manama");
   let manamaDateElement = manamaElement.querySelector(".date");
   let manamaTimeElement = manamaElement.querySelector(".time");
@@ -18,5 +19,23 @@ function updateTime() {
   manamaTimeElement.innerHTML = manamaTime.format("HH:mm:ss");
 }
 
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace(`_`, ` `).split(`/`)[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElememnt = document.querySelector("#cities");
+  citiesElememnt.innerHTML = `<div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("dddd, MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format("HH:mm:ss")}</div>
+        </div>`;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+
+citiesSelectElement.addEventListener("change", updateCity);
